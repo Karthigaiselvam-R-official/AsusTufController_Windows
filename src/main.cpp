0d6ff6ef9808723c5d688dd6ad3ea633bf4e2c29
Karthigaiselvam-R-official <karthigaiselvamr.cs2022@gmail.com>
2026-02-09 16:52:20 +0530

    Update main entry point and QML registration

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,72 @@
 
+#include "AuraController.h"
+#include "FanController.h"
+#include "FanCurveController.h"
+#include "LanguageController.h"
+#include "SystemStatsMonitor.h"
+#include "ThemeController.h"
 #include <QGuiApplication>
+#include <QIcon>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
-#include <QIcon>
-#include "FanController.h"
-#include "AuraController.h"
-#include "SystemStatsMonitor.h"
-#include "FanCurveController.h"
+#include <QQuickStyle>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
 
-    QGuiApplication app(argc, argv);
-    app.setWindowIcon(QIcon("resources/FanControl.png"));
-    app.setOrganizationName("AsusTuf");
-    app.setOrganizationDomain("asus.com");
-    app.setApplicationName("AsusTufFanControl");
+  // Set Qt Quick Controls style to "Basic" to enable custom styling
+  // This MUST be set BEFORE creating QGuiApplication
+  QQuickStyle::setStyle("Basic");
 
-    // Register Types exposed to QML
-    qmlRegisterType<FanController>("AsusTufFanControl", 1, 0, "FanController");
-    qmlRegisterType<AuraController>("AsusTufFanControl", 1, 0, "AuraController");
-    qmlRegisterType<SystemStatsMonitor>("AsusTufFanControl", 1, 0, "SystemStatsMonitor");
-    qmlRegisterType<FanCurveController>("AsusTufFanControl", 1, 0, "FanCurveController");
+  QGuiApplication app(argc, argv);
+  app.setWindowIcon(QIcon("resources/FanControl.png"));
+  app.setOrganizationName("AsusTuf");
+  app.setOrganizationDomain("asus.com");
+  app.setApplicationName("AsusTufFanControl");
 
-    QQmlApplicationEngine engine;
-    // Qt 6 Recommended: Load from Module URI
-    // This allows "import AsusTufFanControl" to resolve correctly inside the main entry
-    const QUrl url("qrc:/AsusTufFanControl/ui/Main.qml");
-    
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
+  // Initialize Controllers (Static instances to persist and link)
+  static LanguageController langController;
+  static ThemeController themeController;
+  static FanController fanController;
+  static FanCurveController fanCurveController;
+
+  // Link Controllers (Crucial Fix: Connect Brain to Hands)
+
+  // Link Controllers (Crucial Fix: Connect Brain to Hands)
+  fanCurveController.setFanController(&fanController);
+
+  // Register Types exposed to QML
+  qmlRegisterType<AuraController>("AsusTufFanControl", 1, 0, "AuraController");
+  qmlRegisterType<SystemStatsMonitor>("AsusTufFanControl", 1, 0,
+                                      "SystemStatsMonitor");
+
+  // Register Singletons
+  qmlRegisterSingletonInstance<LanguageController>(
+      "AsusTufFanControl", 1, 0, "LanguageController", &langController);
+  qmlRegisterSingletonInstance<ThemeController>(
+      "AsusTufFanControl", 1, 0, "ThemeController", &themeController);
+  qmlRegisterSingletonInstance<FanController>("AsusTufFanControl", 1, 0,
+                                              "FanController", &fanController);
+  qmlRegisterSingletonInstance<FanCurveController>(
+      "AsusTufFanControl", 1, 0, "FanCurveController", &fanCurveController);
+
+  QQmlApplicationEngine engine;
+  // Qt 6 Recommended: Load from Module URI
+  // This allows "import AsusTufFanControl" to resolve correctly inside the main
+  // entry
+  const QUrl url("qrc:/AsusTufFanControl/ui/Main.qml");
+
+  QObject::connect(
+      &engine, &QQmlApplicationEngine::objectCreated, &app,
+      [url](QObject *obj, const QUrl &objUrl) {
         if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
+          QCoreApplication::exit(-1);
+      },
+      Qt::QueuedConnection);
 
-    engine.load(url);
+  engine.load(url);
 
-    return app.exec();
+  return app.exec();
 }
