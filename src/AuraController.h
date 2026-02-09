c0aefa00159ff1f205c5af9f947dc7a016270977
Karthigaiselvam-R-official <karthigaiselvamr.cs2022@gmail.com>
2026-02-09 16:52:18 +0530

    Update AuraController header

--- src/AuraController.h
+++ src/AuraController.h
@@ -1,34 +1,62 @@
 #ifndef AURACONTROLLER_H
 #define AURACONTROLLER_H
 
+#include <QColor>
 #include <QObject>
-#include <QString>
 #include <QSettings>
+#include <QString>
+#include <QTimer>
 
-class AuraController : public QObject
-{
-    Q_OBJECT
+class AuraController : public QObject {
+  Q_OBJECT
+  Q_PROPERTY(bool connected READ isConnected CONSTANT)
+
 public:
-    explicit AuraController(QObject *parent = nullptr);
+  explicit AuraController(QObject *parent = nullptr);
+  ~AuraController();
 
-    // Invokables called by AuraPage.qml
-    Q_INVOKABLE void setBrightness(int level); // 0-3
-    Q_INVOKABLE void setStatic(QString color); // Hex string "FF0000"
-    Q_INVOKABLE void setBreathing(QString color, int speed);
-    Q_INVOKABLE void setRainbow(int speed);
-    Q_INVOKABLE void setPulsing(QString color, int speed); // Strobing
-    
-    Q_INVOKABLE void saveState(QString mode, QString color);
-    Q_INVOKABLE int getSystemBrightness();
-    Q_INVOKABLE QString getLastMode();
-    Q_INVOKABLE QString getLastColor();
+  Q_INVOKABLE void initFromHardware();
+
+  Q_INVOKABLE void setBrightness(int level);
+  Q_INVOKABLE void setStatic(QString color);
+  Q_INVOKABLE void setBreathing(QString color, int speed);
+  Q_INVOKABLE void setRainbow(int speed);
+  Q_INVOKABLE void setPulsing(QString color, int speed);
+
+  Q_INVOKABLE void saveState(QString mode, QString color, int brightness,
+                             int speed);
+  Q_INVOKABLE QString getLastMode();
+  Q_INVOKABLE QString getLastColor();
+  Q_INVOKABLE int getLastBrightness();
+  Q_INVOKABLE int getLastSpeed();
+  Q_INVOKABLE int getHardwareBrightness();
+  Q_INVOKABLE void restoreServices(QString mode, QString color);
+  Q_INVOKABLE int getSystemBrightness();
+  Q_INVOKABLE bool isConnected();
+
+signals:
+  void modeChanged(QString mode);
+  void brightnessChanged(int level);
+
+private slots:
+  void onStrobeTimeout();
+  void onPollTimeout();
 
 private:
-    void setMode(int mode); // 0: Static, 1: Breathing, 2: Rainbow, 3: Strobe
-    void setColor(QString colorHex);
-    void setSpeed(int speed);
+  QColor parseColor(QString colorHex);
+  void applyMode(QString mode, QString color, int speed);
+  void stopStrobing();
 
-    QSettings *m_settings;
+  QSettings *m_settings;
+  QTimer *m_strobeTimer;
+  QTimer *m_pollTimer;
+  QString m_strobeColor;
+  bool m_strobeToggle;
+
+  // Previous confirmed state (to avoid signal spam)
+  QString m_lastReadMode;
+  int m_lastReadBrightness;
+  bool m_initializedFromSettings;
 };
 
-#endif // AURACONTROLLER_H
+#endif
